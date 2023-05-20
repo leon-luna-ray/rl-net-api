7e0c72a26a928df3a51b40f7208d04b65625e29c
Ray Luna <leon.luna.ray@gmail.com>
2023-05-19 20:32:15 -0700

    add size options to image grid block

--- apps/base/models/blocks/grid.py
+++ apps/base/models/blocks/grid.py
@@ -2,17 +2,32 @@ from wagtail.blocks import (
     CharBlock,
     StreamBlock,
     StructBlock,
+    ChoiceBlock,
 )
 from apps.base.serializers import ApiImageChooserBlock
 
+# todo rm
 class LargeImageGridBlock(StructBlock):
     title = CharBlock()
     images = StreamBlock([
         ('image', ApiImageChooserBlock())
     ])
-
+# todo rm
 class SmallImageGridBlock(StructBlock):
     title = CharBlock()
+    images = StreamBlock([
+        # Todo add choice to select collection as well.
+        ('image', ApiImageChooserBlock())
+    ])
+
+class ImageGridBlock(StructBlock):
+    grid_sizes = [
+        ('small', 'Small'),
+        ('medium', 'Medium'),
+        ('large', 'Large'),
+    ]
+    title = CharBlock()
+    grid_size = ChoiceBlock(choices=grid_sizes, default='large')
     images = StreamBlock([
         ('image', ApiImageChooserBlock())
     ])
