fb86ed9a701932cc7c67ab1d724154d864f71b84
Ray Luna <leon.luna.ray@gmail.com>
2023-07-01 09:46:39 -0700

    clear console errors

--- apps/base/snippets.py
+++ apps/base/snippets.py
@@ -1,6 +1,6 @@
 """Register Collection snippet."""
 from wagtail.snippets.models import register_snippet
-from wagtail.core.models import Collection
+from wagtail.models import Collection
 
 # Register Collections as Snippets so we can use the SnippetChooserPanel to select a collection
 register_snippet(Collection)
